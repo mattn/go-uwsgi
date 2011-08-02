b8deee359c1ab6f1c0449b9b7e0ac2ae981d8cea
mattn <mattn.jp@gmail.com>
2011-08-02 15:46:04 +0900

    first import.

---
+++ uwsgi.go
@@ -0,0 +1,165 @@
+/*
+This file implements the uWSGI protocol.
+This implements run as net.Listener:
+
+
+		l, err = net.Listen("unix", "/path/to/socket")
+		http.Serve(&UwsgiListener{l}, http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+			w.Header().Set("Content-Length", 11)
+			w.Write([]byte("hello world"))
+		})
+*/
+
+package uwsgi
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"io"
+	"net"
+	"os"
+	"strconv"
+)
+
+type Listener struct {
+	Listener net.Listener
+}
+
+type Conn struct {
+	env    map[string]string
+	reader io.Reader
+	writer net.Conn
+}
+
+func (c *Conn) Read(b []byte) (int, os.Error) {
+	n, e := c.reader.Read(b)
+	return n, e
+}
+
+func (c *Conn) Write(b []byte) (int, os.Error) {
+	return c.writer.Write(b)
+}
+
+func (c *Conn) Close() os.Error {
+	return c.writer.Close()
+}
+
+func (c *Conn) LocalAddr() net.Addr {
+	return c.writer.LocalAddr()
+}
+
+func (c *Conn) RemoteAddr() net.Addr {
+	return c.writer.RemoteAddr()
+}
+
+func (c *Conn) SetTimeout(s int64) os.Error {
+	return c.writer.SetTimeout(s)
+}
+
+func (c *Conn) SetReadTimeout(s int64) os.Error {
+	return c.writer.SetReadTimeout(s)
+}
+
+func (c *Conn) SetWriteTimeout(s int64) os.Error {
+	return c.writer.SetWriteTimeout(s)
+}
+
+func (l *Listener) Addr() net.Addr {
+	return l.Listener.Addr()
+}
+
+func (l *Listener) Close() os.Error {
+	return l.Listener.Close()
+}
+
+// Accept conduct as net.Listener. uWSGI protocol is working good for CGI.
+// This function parse headers and pass to the Server.
+func (l *Listener) Accept() (net.Conn, os.Error) {
+	fd, err := l.Listener.Accept()
+	if err != nil {
+		return nil, err
+	}
+
+	buf := new(bytes.Buffer)
+	c := &Conn{map[string]string{}, buf, fd}
+
+	var head [4]byte
+	fd.Read(head[:])
+	//mod1 := head[0:0]
+	b := []byte{head[1], head[2]}
+	envsize := binary.LittleEndian.Uint16(b)
+	//mod2 := head[3:3]
+
+	envbuf := make([]byte, envsize)
+	if _, err := io.ReadFull(fd, envbuf); err != nil {
+		return nil, err
+	}
+
+	i := uint16(0)
+	for {
+		b := []byte{envbuf[i], envbuf[i+1]}
+		kl := binary.LittleEndian.Uint16(b)
+		i += 2
+		k := string(envbuf[i : i+kl])
+		i += kl
+		b = []byte{envbuf[i], envbuf[i+1]}
+		vl := binary.LittleEndian.Uint16(b)
+		i += 2
+		v := string(envbuf[i : i+vl])
+		i += vl
+
+		c.env[k] = v
+		if i >= envsize {
+			break
+		}
+	}
+
+	//fmt.Fprintf(buf, "%s %s %s\r\n", c.env["REQUEST_METHOD"], c.env["REQUEST_URI"], c.env["SERVER_PROTOCOL"])
+	fmt.Fprintf(buf, "%s %s %s\r\n", c.env["REQUEST_METHOD"], c.env["REQUEST_URI"], "HTTP/1.0")
+
+	cl, _ := strconv.Atoi64(c.env["CONTENT_LENGTH"])
+	if cl > 0 {
+		fmt.Fprintf(buf, "Content-Length: %d\r\n", cl)
+	}
+
+	if v := c.env["HTTP_CONTENT_TYPE"]; len(v) > 0 {
+		fmt.Fprintf(buf, "Content-Type: %s\r\n", v)
+	}
+
+	if v := c.env["HTTP_HOST"]; len(v) > 0 {
+		fmt.Fprintf(buf, "Host: %s\r\n", v)
+	}
+
+	if v := c.env["HTTP_CONNECTION"]; len(v) > 0 {
+		fmt.Fprintf(buf, "Connection: %s\r\n", v)
+	}
+
+	if v := c.env["HTTP_ACCEPT"]; len(v) > 0 {
+		fmt.Fprintf(buf, "Accept: %s\r\n", v)
+	}
+
+	if v := c.env["HTTP_USER_AGENT"]; len(v) > 0 {
+		fmt.Fprintf(buf, "User-Agent: %s\r\n", v)
+	}
+
+	if v := c.env["HTTP_ACCEPT_ENCODING"]; len(v) > 0 {
+		fmt.Fprintf(buf, "Accept-Encoding: %s\r\n", v)
+	}
+
+	if v := c.env["HTTP_ACCEPT_LANGUAGE"]; len(v) > 0 {
+		fmt.Fprintf(buf, "Accept-Language: %s\r\n", v)
+	}
+
+	if v := c.env["HTTP_ACCEPT_CHARSET"]; len(v) > 0 {
+		fmt.Fprintf(buf, "Accept-Charset: %s\r\n", v)
+	}
+
+	buf.Write([]byte("\r\n"))
+
+	if cl > 0 {
+		io.Copyn(buf, fd, cl)
+	}
+
+	return c, nil
+}
---
+++ uwsgi_test.go
@@ -0,0 +1,100 @@
+package uwsgi
+
+import (
+	"bufio"
+	"http"
+	"net"
+	"encoding/binary"
+	"io"
+	"io/ioutil"
+	"fmt"
+	"testing"
+	"syscall"
+)
+
+func writeKV(fd io.Writer, k, v string) {
+	var b [2]byte
+	binary.LittleEndian.PutUint16(b[:], uint16(len(k)))
+	fd.Write(b[:])
+	fd.Write([]byte(k))
+	binary.LittleEndian.PutUint16(b[:], uint16(len(v)))
+	fd.Write(b[:])
+	fd.Write([]byte(v))
+}
+
+func TestServer(t *testing.T) {
+	l, err := net.Listen("tcp", "127.0.0.1:0")
+	if err != nil {
+		t.Fatalf("listen error: %v", err)
+	}
+	addr, _ := l.Addr().(*net.TCPAddr)
+
+	var lastReq *http.Request
+	reqNum := 0
+	handler := http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
+		reqNum++
+
+		v := fmt.Sprintf("bar%d", reqNum)
+		if req.FormValue("foo") == v {
+			fmt.Fprintf(res, "req=%d", reqNum)
+		}
+		lastReq = req
+	})
+
+	server := &http.Server{Handler: handler}
+	go server.Serve(&Listener{l})
+
+	m := map[string]string{
+		"REQUEST_METHOD":  "POST",
+		"REQUEST_URI":     "/foo",
+		"CONTENT_LENGTH":  "8",
+		"CONTENT_TYPE":    "text/plain",
+		"SERVER_PROTOCOL": "HTTP/1.1",
+		"HTTP_USER_AGENT": "go",
+	}
+	var b [2]byte
+	var head [4]byte
+	for n := 1; n <= 3; n++ {
+		fd, _ := net.Dial("tcp", addr.String())
+		s := 0
+		for k, v := range m {
+			s += (len([]byte(k)) + len([]byte(v)) + 4)
+		}
+		binary.LittleEndian.PutUint16(b[:], uint16(s))
+		head[1] = b[0]
+		head[2] = b[1]
+		fd.Write(head[:])
+		for k, v := range m {
+			writeKV(fd, k, v)
+		}
+		fmt.Fprintf(fd, "foo=bar%d", n)
+		syscall.Sleep(1e9)
+
+		res, _ := http.ReadResponse(bufio.NewReader(fd), lastReq)
+		got := res.Request.Method
+		expected := "POST"
+		if string(got) != expected {
+			t.Errorf("Unexpected response for request #1; got %q; expected %q",
+				string(got), expected)
+		}
+
+		got = res.Request.RawURL
+		expected = "/foo"
+		if string(got) != expected {
+			t.Errorf("Unexpected response for request #1; got %q; expected %q",
+				string(got), expected)
+		}
+		body, _ := ioutil.ReadAll(res.Body)
+		res.Body.Close()
+		got = string(body)
+		expected = fmt.Sprintf("req=%d", n)
+		if string(got) != expected {
+			t.Errorf("Unexpected response for request #1; got %q; expected %q",
+				string(got), expected)
+		}
+		fd.Close()
+		fd = nil
+	}
+
+	l.Close()
+}
